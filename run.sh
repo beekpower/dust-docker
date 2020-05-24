3675a0eef29bf59f173c505f17fa37b6926cc06d
nicholas <nicholas@pop-os.localdomain>
2020-05-24 01:13:07 -0400

    update sh script

--- run.sh
+++ run.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-# if [ $1 = "dust" ]
-#   then
-#     echo "No argument supplied"
-# fi
-
-# cp /dust/dust/build/bin/dust /src/dust
-cd /src && /dust/dust/build/bin/dust
+if [ $1 = "dust" ]
+then
+    shift 
+    cd /src && /dust/dust/build/bin/dust $@
+elif [ $1 = "dust_pre" ]
+then
+    shift 
+    cd /src && /dust/dust/build/bin/dust_pre $@
+elif [ $1 = "dust_post" ]
+then
+    shift 
+    cd /src && /dust/dust/build/bin/dust_post $@
+else
+   echo "None of the condition met"
+fi
 
